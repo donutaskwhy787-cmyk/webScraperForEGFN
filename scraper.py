31e3845994c0ec41d871100ed0e5ce8e731623ff
donutaskwhy787-cmyk <donutaskwhy787@gmail.com>
2026-04-10 01:02:37 -0400

    uhh lets fix this

--- scraper.py
+++ scraper.py
@@ -7,15 +7,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=new')
+
+driver = webdriver.Chrome(options=options)
 driver.get("https://www.cbioportal.org/study/clinicalData?id=lgggbm_tcga_pub")
 
-time.sleep(3)
-
-page_source = driver.page_source
+time.sleep(.4)
 
 driver.quit()
-
+page_source = driver.page_source
 soup = BeautifulSoup(page_source, "lxml")
 
-print(soup.prettify)
+table = soup.find("table", class_ = "simple-table table table-striped table-border-top")
+headers = table.find_all("th")
+titles = []
+
+for i in headers:
+    title = i.text
+    titles.append(title)
+
+df = pd.DataFrame(columns=titles)
+print(df)
