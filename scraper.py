e49b1421c682ad466145c58581ed03bee632c9ac
donutaskwhy787-cmyk <donutaskwhy787@gmail.com>
2026-04-10 01:57:54 -0400

    ok so were close rn

--- scraper.py
+++ scraper.py
@@ -11,17 +11,16 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless=new')
 
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.cbioportal.org/study/clinicalData?id=lgggbm_tcga_pub")
+driver.get("https://www.cbioportal.org/patient?studyId=lgggbm_tcga_pub&caseId=TCGA-06-0125")
 
 WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.simple-table")))
 
 page_source = driver.page_source
 
-
-
 soup = BeautifulSoup(page_source, "lxml")
 
-tabler = soup.find("table", class_ = "simple-table table table-striped table-border-top")
+tablery = soup.find_all("table", class_ = "simple-table table table-striped table-border-top")
+tabler = tablery[1]
 headers = tabler.find_all("th")
 titles = []
 
@@ -29,5 +28,12 @@ for i in headers:
     title = i.text
     titles.append(title)
 
-print(titles)
+df = pd.DataFrame(columns=titles)
+
+rows = tabler.find_all("tr")
+for i in rows[1:]:
+    data = i.find_all("td")
+    row = [tr.text for tr in data]
+    df.loc[len(df)] = row
+print(df)
 driver.quit()
